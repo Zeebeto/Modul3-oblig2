8fe2a6f680c1441697f8bee8fd2fb21186728346
Zeebeto <72790252+Zeebeto@users.noreply.github.com>
2021-03-29 13:59:16 +0200

    Initial commit

---
+++ Oblig2/Rook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oblig2
+{
+    class Rook : Piece
+    {
+        public Rook() : base("TRN")
+        {
+
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var diffCol = fromPosition[0] - toPosition[0];
+            var diffRow = fromPosition[1] - toPosition[1];
+            return Math.Abs(diffRow) == Math.Abs(diffCol);
+        }
+    }
+}
